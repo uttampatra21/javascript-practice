7f509672dcd599e42590427744d243c9b21007af
uttam patra <dev.uttampatra@gmail.com>
2022-09-30 12:44:21 +0530

    initial commit'

---
+++ index.js
@@ -0,0 +1,473 @@
+
+// if(score = 0){
+//     console.log("we won the game");
+// }else{
+//     console.log("we lose the game");
+// }
+
+// var age = prompt("Enter your age");
+// if(age>=18){
+//     console.log("You are elegible for vote");
+// } else{
+//     console.log("You are not elegible for vote");
+// }
+
+// var age = prompt("Enter your age");
+// console.log((age>=18) ? "you can vote" : "you can't vote");
+
+// var weather = prompt("What is the weather condition: ");
+// console.log((weather === "rain") ? "Take Raincoat" : "No need to take Raincoat");
+
+// var area = prompt("ENTEr") ;
+// if(area == "cercle"){
+//     console.log("the area of "+ area + " is valid");
+// }else if(area == "triangel"){
+//     console.log("the area of "+ area + " is valid");
+// }else if(area == "rectangle"){
+//     console.log("the area of "+ area + " is valid");
+// }else{
+//     console.log("the area of "+ area + " not is valid");
+// }
+
+// var myage = prompt("Enter your age");
+// if(myage >= 18){
+//     console.log("you are eligible");
+// }else{
+//     console.log("you are not eligible");
+// }
+
+// let weather = "sunny";
+// if(weather === "rain"){
+//     console.log("This the time to take umbrella");
+// }else{
+//     console.log("Take raincoat");
+// }
+
+
+// var age = prompt(12);
+//  age = (age >= 18) ? "you are eligible" : "you are not eligible" ;
+//  console.log(age);
+
+
+
+
+// TURNERRY OPPRETOR:::::::: 
+// turnarry operator is the easy way of if else stetment::::::
+
+
+
+
+
+
+
+
+//  let weather = prompt("Enter the weather condition:");
+//  weather = (weather === "rainny") ? "Take raincoat" : "Take umbrella";
+// alert(weather);
+
+// var myAge = prompt("Enter your age:");
+// myAge = (myAge >= 18) ? "You are eligible" : "you are not eligible";
+// console.log(myAge);
+
+// checking weather using turnerry oppretor::
+
+// var weather = prompt("Enter the weather condition :");
+
+// weather = (weather == "sunny") ? "Take an umbrella" : "Take raincoat";
+// alert(weather);
+
+
+
+
+    // SWITCH CASE:::::::: 
+
+
+// Switch case is the easy stetment then if else ::
+// exp: 
+
+
+// let area = prompt("Eneter the value of an area:");
+// switch(area){
+//     case "circle" : alert("this is the circle");
+//     break;
+
+//     case "trinangle" : alert("This is the triangle");
+//     break;
+
+//     case "rectangle" : alert("This is the rectangle");
+//     break;
+
+//     default : alert("Please enter the valid data");
+// }
+
+// var area = prompt("enter the data");
+// switch(area){
+//     case 'circle' : console.log("this is the circle");
+//     break;
+
+//     case 'triengle' : console.log("this the triengle");
+//     break;
+
+//     case 'rectangle' : console.log("this the rectangle");
+//     break;
+
+//     default : console.log("Enter the valid data");
+    
+
+// }
+
+
+// var area = prompt("Enter:");
+// switch(area){
+//     case 'circle' : 
+//     console.log("the area of "+ area + " is valid");
+//     break;
+
+//     case 'triengle' : 
+//     console.log("the area of "+ area + " is valid");
+//     break;
+
+//     case 'rectangle' : 
+//     console.log("the area of "+ area + " is valid");
+//     break;
+
+//     default:
+//         console.log("Enter the valid data!");
+// }
+
+
+// var weather = prompt("Enter the weather :");
+// switch(weather){
+//     case 'rainny' : 
+//     console.log("You should take raincoat");
+
+//     default : 
+//     console.log("No need to take raincoat");rainn
+// }
+
+
+
+// ..........................LOOPS DURATOIN: 2:50:00
+
+
+// WHILE LOOP
+
+
+
+
+// var number = 0;
+// while(number <= 10){
+//     console.log(number);
+//     number++;
+// }
+
+
+// let number = 1;
+// while(number <= 10){
+
+//     console.log(number);
+
+//     number++;
+// }
+
+
+
+// let number = 1;
+
+// while(number <= 10){
+//     var tableOf = 8;
+//     console.log(tableOf + " * " + number + " = " + tableOf*number);
+//     number++;
+// }
+
+// 
+// var number = 0;
+// while(number <= 10){
+//     console.log(number);
+//     number++;
+// }
+
+// var num = 0;
+// while(num <= 8){
+//     console.log(num);
+//     num++;
+// }
+
+
+// DO WHILE LOOP
+//  do-while loop structure is :: 
+
+
+// var number = 1;
+// do{
+//     console.log(number);
+//     number++;
+// }
+// while(number <= 14);
+
+
+
+
+// let num = 1;
+// let tableOf = 4;
+// do{
+//     console.log(tableOf +" * " +num + " = " + tableOf*num);
+//     num++;
+// }while(num <= 10);
+
+
+
+// let num = 0;
+//  do{
+//     console.log(num)
+//     num++;
+// }while(num <= 10);
+
+
+// let number = 0;
+
+// do{
+//     console.log(number);
+//     number++;
+// }while(number <= 10)
+
+
+// var num = 0;
+
+// do{
+//     console.log(num);
+//     num++;
+// }while(num <= 10);
+
+// var number = 0;
+
+// do{
+//     console.log(number)
+//     number++;
+// }while(number <= 100);
+//  var num = 0;
+//  do{
+//     console.log(num);
+//     num++;
+//  }
+//  while(num <= 10);
+
+// switch case is the easy way of if else stetment
+
+
+// while loop 
+
+
+
+// var num = 0; 
+// while(num <= 10){
+//     console.log(num);
+//     num++;
+// }
+// var num = 0;
+// do{
+//     console.log(num);
+//     num++;
+// }
+// while(num <= 20);
+
+
+// FOR LOOP IS EASY WAY FOR WHILE AND DO WHILE LOOP
+
+
+// for(var num = 1; num <= 10; num++){
+//     var tableOf = 2;
+//     console.log(tableOf + " * "+  num);
+// }
+
+
+// for(var num = 1; num <=10; num++){
+//     var tableOf = 9;
+//     console.log(tableOf + " * " + num + " = " + tableOf*num);
+// }
+
+// for(var num = 1; num <=40; num++){
+//     var tableOf = 8;
+//     console.log(tableOf + " * " + num + " = " + tableOf*num);
+// }
+
+// JAVASCRIPT FUNCTIONS duration: 3:16:00
+
+
+// var myName = prompt("Enter your name:");
+// myName = myName.toUpperCase();
+// alert( "hello " + myName);
+
+
+// function sum (){
+//     var a = 4; b = 3;
+//     var total = a+b;
+//     console.log(total);
+// }
+// sum();
+
+//Reterun keyword      duration: 3:39: 00
+
+/// ECMA SCRIPT
+
+// durration:: 3:47:00
+
+
+// function 
+
+// function biodata(){
+//     let myfstName = "vinod";
+//     console.log(myfstName);
+
+//     if(true){
+//         myfstName = "londa"
+//         console.log(myfstName);
+//     }
+// }
+// // biodata();
+//     function biodata (){
+//         let a=4; b=3;
+//         let total = a+b;
+//         console.log(total);
+//     }
+//     biodata();
+
+//function practice::::
+
+// function sum(){
+//     a = 12;
+//     b = 12;
+//     var total = a+b;
+//     console.log(total);
+// }
+// sum();
+
+
+
+
+
+
+
+
+
+
+// temPleat literals:::::
+
+    // for(let num = 1; num <= 10; num++){
+    //     let tableOf = 3;
+    //     console.log(`${tableOf} * ${num} = ${tableOf*num}`);
+    // }
+
+// for(let num = 1; num <= 10; num++){
+//     let tableOf = 5;
+//     console.log(`${tableOf} * ${num} = ${tableOf * num}`)
+// }
+
+
+
+// for(let num = 1; num <= 10; num++){
+//     let tableOf = 5;    
+//     console.log(`${tableOf} * ${num} = ${tableOf*num}`);
+// }
+
+
+
+// default arguments:::
+
+// function total(a,b=5){
+//     return a*b;
+// }
+// console.log(total(6));
+
+/// fat arrow:::::::
+// in fat arrow we can't call the function before define::
+// this is the traditional function where you can call the function before define ::
+
+
+// console.log(sum());
+// function sum(){
+//     let a = 2, b = 4;
+//     let sum = a+b;
+//     return `the sum of the number is ${sum}`;
+// }
+
+
+
+
+
+// THIS IS FAT ARROW FUNCTION::::::::::
+
+//:: fat arrow function is easy way for writing any function 
+// it helps us to write one line code only::: 
+
+
+
+// const sum = () => `The sum of the two number is ${(5)+(9)}`
+// console.log(sum());
+
+// const sum = () => `The sum of two number is ${(a=10)+(b=10)}`
+// console.log(sum());
+
+// const sum = () => `the sum of the number is ${(a=3)+(b=3)}`;
+// console.log(sum());
+
+//  const sum = () => `The sum of the two number is ${(a=10)+(b=10)}`
+//  console.log(sum());
+
+// const sum = () => `the sum of the two number is ${(a=10)+(b=20)}`
+// console.log(sum());
+
+
+// let a = 20;
+// let b = 30;
+// const sum = () => `The sum of the two number is ${a+b}`
+// console.log(sum());
+
+
+
+//ARRAY IN JAVASCRIPT::::
+
+
+ // array is where you can store multiple data:::::
+//exp:::
+// var names = [ "uttam", "patra", "lawda", "lasson"]
+
+
+
+
+// 01 -- traversal of an array:::::::
+// var names = [ "uttam", "patra", "lawda", "lasson"];
+// console.log(names[ names.length - 1]);
+
+// var names =["uttam", "patra", "loda"];
+// console.log(names);
+// console.log(names [names.length -1 ] );
+
+
+// var names = [ "ramesh", "uttam", "rahul", "pratik"]
+// console.log(names[0]);
+
+
+// for in loop we use to nagivate:::
+
+// var names = [ "uttam", "adrak", "albert", "serifn"];
+
+// for(i = 0; i < names.length; i++){
+//     console.log(names[i]);
+// }
+
+
+// var names = ["uttam", "adrak", "albert", "serifn"];
+// for(i = 0; i < names.length; i++){
+//     console.log(names[i]);
+// }
+
+// var names = ["ramesh", "uttam", "rahul", "pratik"];
+// for (i = 0; i < names.length ; i++){
+// console.log(names[i]);
+// }
+
+// duration: 4:42:00
+
+const sum=()=> `the sum of the  two number is ${(a= 21)+(b=43)}`
+console.log(sum());
